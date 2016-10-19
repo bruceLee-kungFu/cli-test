b14f824b30ee8012d0dff0676980509d78dd93da
Bruce Lee <brucelee4711@googlemail.com>
2016-10-19 18:35:25 +0200

    Initialize repository

---
+++ app/assets/javascripts/footer.js
@@ -0,0 +1,37 @@
+
+//= require f5_js/foundation
+//= require f5_js/foundation.abide
+//= require f5_js/foundation.accordion
+//= require f5_js/foundation.alert
+//= require f5_js/foundation.clearing
+//= require f5_js/foundation.dropdown
+//= require f5_js/foundation.equalizer
+//= require f5_js/foundation.interchange
+//= require f5_js/foundation.joyride
+//= require f5_js/foundation.magellan
+//= require f5_js/foundation.offcanvas
+//= require f5_js/foundation.orbit
+//= require f5_js/foundation.reveal
+//= require f5_js/foundation.slider
+//= require f5_js/foundation.tab
+//= require f5_js/foundation.tooltip
+//= require f5_js/foundation.topbar
+
+// f5_js/foundation.abide.js
+// f5_js/foundation.accordion.js
+// f5_js/foundation.alert.js
+// f5_js/foundation.clearing.js
+// f5_js/foundation.dropdown.js
+// f5_js/foundation.equalizer.js
+// f5_js/foundation.interchange.js
+// f5_js/foundation.joyride.js
+// f5_js/foundation.js
+// f5_js/foundation.magellan.js
+// f5_js/foundation.offcanvas.js
+// f5_js/foundation.orbit.js
+// f5_js/foundation.reveal.js
+// f5_js/foundation.slider.js
+// f5_js/foundation.tab.js
+// f5_js/foundation.tooltip.js
+// f5_js/foundation.topbar.js
+
